ebb0ebb1dfadab62ef66866bf0255b6fd9748354
chohyomin <whgyals01@naver.com>
2024-12-05 03:37:50 +0900

    사용자 입력에 대한 유효성 검사 추가

--- HanoiTower/hanoi.cpp
+++ HanoiTower/hanoi.cpp
@@ -13,8 +13,15 @@ void hanoi(int n, char from, char to, char aux) {
 
 int main() {
     int n;
-    std::cout << "Enter the number of disks: ";
-    std::cin >> n; // 사용자 입력
+    // 사용자 입력 및 유효성 검사
+    do {
+        std::cout << "Enter the number of disks (positive integer): ";
+        std::cin >> n;
+        if (n <= 0) {
+            std::cout << "Please enter a positive integer." << std::endl;
+        }
+    } while (n <= 0); // 유효하지 않은 경우 반복
+
     std::cout << "Moves to solve the Towers of Hanoi with " << n << " disks:" << std::endl;
     hanoi(n, 'A', 'C', 'B'); // A, B, C는 기둥 이름
     return 0;
