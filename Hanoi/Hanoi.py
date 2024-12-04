219701acfc0ce3c54d65b0f02811729ffb49dddf
chohyomin <whgyals01@naver.com>
2024-12-05 03:46:05 +0900

    기본 하노이탑 구현 코드 추가

---
+++ Hanoi/Hanoi.py
@@ -0,0 +1,12 @@
+
+def hanoi(n, from_rod, to_rod, aux_rod):
+    if n == 1:
+        print(f"Move disk 1 from {from_rod} to {to_rod}")
+        return
+    hanoi(n - 1, from_rod, aux_rod, to_rod)
+    print(f"Move disk {n} from {from_rod} to {to_rod}")
+    hanoi(n - 1, aux_rod, to_rod, from_rod)
+
+if __name__ == "__main__":
+    n = 3  # 디스크 수
+    hanoi(n, 'A', 'C', 'B')  # A, B, C는 기둥 이름
